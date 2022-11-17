b3789985810c732e0551f162052b4adff7d25f99
Anastasiya Sakharuk <sakharuk333@gmail.com>
2022-11-17 09:50:32 +0300

    Average

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -6,7 +6,18 @@ public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Use Scanner methods to read input
-
+        int count = 0;
+        int currValue;
+        int sum = 0;
+        do {
+            currValue = scanner.nextInt();
+            sum += currValue;
+            count ++;
+        } while (currValue != 0);
+//        System.out.println(sum);
+//        System.out.println(count-1);
+        int average = sum / (count-1);
+        System.out.println(average);
     }
 
 }
